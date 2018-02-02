f9e9e842b062917dce33b5ad81a2e3a87465bb21
Jonathan Gimeno <jgimeno@gmail.com>
2018-02-02 17:54:48 +0100

    We can add multiple listeners.

--- dispatcher/dispatcher.go
+++ dispatcher/dispatcher.go
@@ -15,23 +15,24 @@ type EventDispatcher interface {
 
 func New() EventDispatcher {
 	return &eventDispatcher{
-		eventMap: make(map[string]Listener, 1),
+		eventMap: make(map[string][]Listener, 1),
 	}
 }
 
 type eventDispatcher struct {
-	eventMap map[string]Listener
+	eventMap map[string][]Listener
 	waitGroup sync.WaitGroup
 }
 
 func (e *eventDispatcher) Subscribe(eventName string, listener Listener) {
-	e.eventMap[eventName] = listener
+	e.eventMap[eventName] = append(e.eventMap[eventName], listener)
 }
 
 func (e *eventDispatcher) Publish(event event.Event) {
-	e.waitGroup.Add(1)
-	l := e.eventMap[event.GetName()]
-	go l(event, &e.waitGroup)
+	for _, l := range e.eventMap[event.GetName()] {
+		e.waitGroup.Add(1)
+		go l(event, &e.waitGroup)
+	}
 }
 
 func (e *eventDispatcher) Close() {
--- dispatcher/dispatcher_test.go
+++ dispatcher/dispatcher_test.go
@@ -27,3 +27,33 @@ func TestItWaitsUntilItFinishesToCloseTheDispatcher(t *testing.T) {
 		t.Fatalf("The Dispatcher has not executed the listener.")
 	}
 }
+
+func TestWeCanSubscribeMultipleListenersToSameEvent(t *testing.T) {
+	d := dispatcher.New()
+
+	eventName := "event.new"
+
+	var executedListener1 bool
+	var executedListener2 bool
+
+	var listener1 dispatcher.Listener = func(event event.Event, w *sync.WaitGroup) {
+		executedListener1 = true
+		w.Done()
+	}
+
+	var listener2 dispatcher.Listener = func(event event.Event, w *sync.WaitGroup) {
+		executedListener2 = true
+		w.Done()
+	}
+
+	d.Subscribe(eventName, listener1)
+	d.Subscribe(eventName, listener2)
+
+	e := event.New(eventName)
+	d.Publish(e)
+	d.Close()
+
+	if !executedListener1 || !executedListener2 {
+		t.Fatalf("The Dispatcher has not executed all the expected listeners.")
+	}
+}
